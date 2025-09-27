8ab49c53fb59108db1578949c957a4b6a21670b2
Sertavr <pampuh.net@gmail.com>
2025-09-27 14:17:03 +0300

    constant names changed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const DAYLI_RATE = 40;
-  const LONG_TERM = 7;
-  const SHORT_TERM = 3;
+  const DAILY_RATE = 40;
+  const LONG_TERM_THRESHOLD = 7;
+  const SHORT_TERM_THRESHOLD = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const basePrice = days * DAYLI_RATE;
+  const basePrice = days * DAILY_RATE;
 
-  if (days >= LONG_TERM) {
+  if (days >= LONG_TERM_THRESHOLD) {
     return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= SHORT_TERM && days < LONG_TERM) {
+  if (days >= SHORT_TERM_THRESHOLD && days < LONG_TERM_THRESHOLD) {
     return basePrice - SHORT_TERM_DISCOUNT;
   }
 
