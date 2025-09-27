0466f6b85800b3a861f715395e7c847f58638ed4
Sertavr <pampuh.net@gmail.com>
2025-09-27 13:45:49 +0300

    added code writing style practices

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,10 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentalCost = 40;
-  const discount = days >= 3 && days < 7 ? 20 : days >= 7 ? 50 : 0;
+  const DAYLI_RATE = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  return rentalCost * days - discount;
+  const basePrice = days * DAYLI_RATE;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
